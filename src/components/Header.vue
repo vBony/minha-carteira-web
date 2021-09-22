7cdfa2e79ba95c4170302225fd1ea5b18c3ad094
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-09-22 17:28:50 -0300

    redirecionamentos do menu para dashboard

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?= $base_url ?>">
+        <router-link to="/" class="navbar-brand">
             <div class="fs-4 bg-success text-white px-2 py-1 rounded-circle"> <i class="fas fa-money-bill-wave-alt text-white"></i></div>
-        </a>
+        </router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="<?= $base_url ?>">Dashboard</a>
+                    <router-link to="/" class="nav-link active" aria-current="page">Dashboard</router-link>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Sobre</a>
